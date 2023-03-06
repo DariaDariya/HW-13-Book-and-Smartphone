080c9ec80e8d990c99e02f97d974ab6afb88ee86
Daria Test <dashik_gem@icloud.com>
2023-03-07 01:15:18 +0300

    Initial commit

---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,39 @@
+public class ProductRepository {
+    private Product[] items = new Product[0];
+
+    public void save(Product item) {
+        Product[] tmp = new Product[items.length + 1];
+        for (int i = 0; i < items.length; i++) {
+            tmp[i] = items[i];
+        }
+        tmp[tmp.length - 1] = item;
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+
+
+    public void removeById(int id) {
+        Product[] tmp = new Product[items.length - 1];
+        int copyToIndex = 0;
+        for (Product item : items ) {
+            if (item.getId() != id) {
+                tmp[copyToIndex] = item;
+                copyToIndex++;
+            }
+        }
+        items = tmp;
+
+    }
+
+    public Product[] getItems() {
+        return items;
+
+    }
+    public Product[] setItems(Product[] items) {
+        return items;
+
+    }
+}
